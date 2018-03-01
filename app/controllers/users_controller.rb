13d3488161ce323c3faed4dcf68641af277abec9
mevanstron <michael@michaelgregoryevans.com>
2018-03-01 14:33:04 -0600

    changed field name from 'password_confirm' to 'pasword_confirmation' to fit rails convention. added validations for email, password, password_confirmation to User model. updated notes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :nickname, :password, :password_confirm)
+    params.require(:user).permit(:email, :nickname, :password, :password_confirmation)
   end
 
   def set_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   has_many :reviews
 
   has_secure_password
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
+  validates :password, presence: true
+  validates :password, confirmation: true
+
+  validates :password_confirmation, presence: trues
 end
