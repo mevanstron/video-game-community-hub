8a40aede2c6648ce2f7197731076c4f7040422a4
mevanstron <michael@michaelgregoryevans.com>
2018-03-01 10:39:24 -0600

    updated notes, coded controller content for #create, #destroy. added has_secure_password to User model.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:email])
+    return head(:forbidden) unless @user.authenticate(params[:password])
+    session[:user_id] = @user.id
   end
 
   def destroy
+    session.delete :user_id
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :video_games
   has_many :reviews
+
+  has_secure_password
 end
