2e8d8f5604c0fb0fdee1e9d0a53799d87b7f39b9
mevanstron <michael@michaelgregoryevans.com>
2018-03-01 14:14:15 -0600

    replaced stubbed content with rails form in users/new.html.erb. updated notes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirm, :nickname)
+    params.require(:user).permit(:email, :nickname, :password, :password_confirm)
   end
 
   def set_user
