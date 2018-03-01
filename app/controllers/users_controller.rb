8cb78d3538ec35be63f5e22d83ce14a1a1c62935
mevanstron <michael@michaelgregoryevans.com>
2018-03-01 14:02:45 -0600

    updated UsersController: #new, #create, #user_params, #set_user. added before_action call. updated notes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    redirect_to user_path(@user)
   end
 
   def show
@@ -15,5 +19,15 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirm, :nickname)
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:id])
   end
 end
