001a98543705119e617fec38c768b290402f175f
mevanstron <michael@michaelgregoryevans.com>
2018-03-02 17:10:08 -0600

    added 'Add to Collection' link on video_games index page.  converted unordered list to table to improve visual. added create and destroy methods to user_video_games controller. added content to create method. updated notes.

--- app/controllers/user_video_games_controller.rb
+++ app/controllers/user_video_games_controller.rb
@@ -1,2 +1,9 @@
 class UserVideoGamesController < ApplicationController
+  def create
+    @user_video_game = UserVideoGame.create(user_id: current_user.id , video_game_id: params[:video_game_id])
+    redirect_to user_path(current_user)
+  end
+
+  def destroy
+  end
 end
