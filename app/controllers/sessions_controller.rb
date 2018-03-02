e2444d7f5d38d1ff626c3dd644de776808495cf0
mevanstron <michael@michaelgregoryevans.com>
2018-03-02 15:16:33 -0600

    updated sessions/new error message content in controller, added call to layouts/_error_message in sessions/new.html.erb.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      flash[:alert] = "Password provided was incorrect."
+      @error_message = "Email / Password combination is incorrect"
       render :new
     end
   end
