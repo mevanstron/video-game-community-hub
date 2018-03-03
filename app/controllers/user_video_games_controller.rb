df741aaae5d5550469db8259d457118c4f8c0f8a
mevanstron <michael@michaelgregoryevans.com>
2018-03-02 18:19:11 -0600

    user_video_game controller: wrote code for #destroy. adjusted redirect from #create to video_games_path. application controller: added #current_user as helper method. refactored user show page html. added a link to add new game on users/show. added text that shows if video game collecti onis empty on users/show.

--- app/controllers/user_video_games_controller.rb
+++ app/controllers/user_video_games_controller.rb
@@ -1,9 +1,11 @@
 class UserVideoGamesController < ApplicationController
   def create
     @user_video_game = UserVideoGame.create(user_id: current_user.id , video_game_id: params[:video_game_id])
-    redirect_to user_path(current_user)
+    redirect_to video_games_path
   end
 
   def destroy
+    UserVideoGame.find_by(user_id: current_user.id , video_game_id: params[:id]).destroy
+    redirect_to user_path(current_user)
   end
 end
