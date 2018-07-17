f3e0f65a763003aded4d6e8265192d604e21981b
mevanstron <michael@michaelgregoryevans.com>
2018-07-17 14:55:11 -0500

    updated spec-js.md

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -6,20 +6,20 @@ function addTag() {
   $('#new_tag').submit(function(event) {
     event.preventDefault();
 
-    var values = $(this).serialize();
-
-    var posting = $.post('/tags', values);
+    let values = $(this).serialize();
+    let posting = $.post('/tags', values);
 
     posting.done(function(data) {
-      console.log(data["name"])
       $("#tag_tbody").append(
         `<tr>
           <td>
             <a href="/tags/${data["id"]}">${data["name"]}</a>
           </td>
-          <td></td>
+          <td>
+          </td>
         </tr>`
       );
     });
   });
+
 }
